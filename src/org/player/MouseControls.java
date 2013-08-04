5481a5855fc5566327dd018195256274cc0dc4f8
drowssaP <maximilianpbloom@gmail.com>
2013-08-04 12:59:55 -0700

    Update MouseControls.java

--- src/org/player/MouseControls.java
+++ src/org/player/MouseControls.java
@@ -1,20 +1,34 @@
-package org.player;
+package testing;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JFrame;
+
+public class Testing extends JFrame implements MouseMotionListener{
+    
+    int my;
+    int mx;
+    
+    @Override
+    public void mouseDragged(MouseEvent e) {
+       mx = e.getX();
+       my = e.getY();
+        mouseDragged = true;
+        
+        
+        e.consume();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+       mx = e.getX();
+       my = e.getY();
+        mouseDragged = false;
+       
+       
+       e.consume();
+    }
 
 
 
-public class MouseControls{
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
