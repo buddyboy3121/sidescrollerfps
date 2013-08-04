f4229ab603573b8ae3c1ad77ceef20fa0d43dcc4
Nacorpio <nacorpio@live.com>
2013-08-04 22:08:27 +0200

    Minor changes
    Added some comments to the WorldRenderer class.
    Nothing too special.

--- src/org/world/WorldRenderer.java
+++ src/org/world/WorldRenderer.java
@@ -4,6 +4,10 @@ import org.newdawn.slick.Graphics;
 
 public class WorldRenderer {
 
+	/**
+	 * Render the world with the specified {@link Graphics} object.
+	 * @param g The graphics object to use for rendering the world.
+	 */
 	public void render(Graphics g) {
 		
 	}
