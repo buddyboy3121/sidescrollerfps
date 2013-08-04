e12281b67ad8be6f7cfc9901bb4ff8775b6c1dc1
buddyboy3121 <buddyboy3121@yahoo.com>
2013-08-04 15:58:00 -0500

    Update Game.java

--- src/org/main/Game.java
+++ src/org/main/Game.java
@@ -4,7 +4,10 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.player.PlayerKeyControls;
 import org.world.LevelData;
+import org.world.WorldRenderer;
+import org.world.WorldUpdater;
 import org.world.server.Generator;
 
 
@@ -12,19 +15,27 @@ public class Game extends BasicGameState {
 
 	public static int screenWidth = 1025;
 	public static int screenHeight = 668;
-	LevelData level = new LevelData();
+	
+	private PlayerKeyControls playerKeyControls;
+	private WorldRenderer worldRenderer;
+	private WorldUpdater worldUpdater;
 	
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		Generator generator = new Generator();
+		playerKeyControls = new PlayerKeyControls();
+		worldRenderer = new WorldRenderer();
+		worldUpdater = new WorldUpdater();
+		
 		generator.generateLevel();
 	}
 	
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		
+		worldRenderer.render(g);
 	}
 
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-		
+		playerKeyControls.update(game, delta);
+		worldUpdater.update();
 	}
 	
 	public int getID() {
