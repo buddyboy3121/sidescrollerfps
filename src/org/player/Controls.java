f8232d8f3dfb0fb99cc309c180d827d57222721d
drowssaP <maximilianpbloom@gmail.com>
2013-08-04 12:16:20 -0700

    Update Controls.java

--- src/org/player/Controls.java
+++ src/org/player/Controls.java
@@ -2,27 +2,6 @@ package org.player;
 
 public class Controls{
  
- public boolean mouseActive = false;
- 
- public mousing(Display f, Images i){
-  f.addKeyListener(new KeyAdapter()
-  public void KeyPressed(KeyEvent e){
-   if(e.getKeyCode() == KeyEvent.VK_M)
-    mousActive = true;
-  });
-  
-  
- }
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
  
  
 }
