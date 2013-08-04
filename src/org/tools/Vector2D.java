fa79773e7fbead795f6271bdff14abd08e1d35ba
Nacorpio <nacorpio@live.com>
2013-08-04 03:06:49 +0200

    Added Vector 2D
    Added a class for storing the coordinates of Players and Entities.

---
+++ src/org/tools/Vector2D.java
@@ -0,0 +1,23 @@
+package org.tools;
+
+public class Vector2D {
+
+	private int x, y;
+	
+	/**
+	 * Returns the X coordinate.
+	 * @return the X coordinate.
+	 */
+	public int getX() {
+		return this.x;
+	}
+
+	/**
+	 * Returns the Y coordinate.
+	 * @return the Y coordinate.
+	 */
+	public int getY() {
+		return this.y;
+	}
+	
+}
