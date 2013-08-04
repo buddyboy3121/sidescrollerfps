e9847768fba133a0d165ef9fb77bca7165a5bc52
buddyboy3121 <buddyboy3121@yahoo.com>
2013-08-03 19:37:29 -0500

    Added the two main classes

---
+++ src/Game.java
@@ -0,0 +1,27 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class Game extends BasicGameState {
+
+	
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+		
+	}
+	
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		g.drawOval(25, 25, 500, 500);
+	}
+
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		
+	}
+	
+	public int getID() {
+		return 0;
+	}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+
+public class Main extends StateBasedGame {
+
+	public Main(String name) {
+		super(name);
+	}
+	
+	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new Game());
+	}
+	
+	public static void main(String args[]) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Main("2d shooter"));
+		
+		app.setDisplayMode(800, 800, false);
+		app.setAlwaysRender(true);
+		app.setTargetFrameRate(60);
+		
+		app.start();
+	}
+
+}
